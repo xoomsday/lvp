aedeed65fc203b7a3cc8ac99ea846e4f59197ba0
Day Xooms <91406401+xoomsday@users.noreply.github.com>
2021-10-04 21:27:24 -0700

    LVP - Local Video Player

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE = 'lvp-cache-0';
+
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
+});
+
+function precache() {
+    return caches.open(CACHE).then(cache => {
+	return cache.addAll([
+            "./LocalVideoPlayer.html",
+            "./sw.js",
+            "./LVP.png",
+            "./manifest.json",
+            "./favicon.ico",
+	]);
+    });
+}
+
+function fromCache(request) {
+    return caches.open(CACHE).then(cache => {
+	return cache.match(request).then(matching => {
+	    return matching || Promise.reject('no-match');
+	});
+    });
+}
+
+function update(request) {
+    return caches.open(CACHE).then(cache => {
+	return fetch(request).then(response => {
+	    return cache.put(request, response);
+	});
+    });
+}
